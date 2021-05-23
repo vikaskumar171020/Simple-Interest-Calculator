e74c341c75a0cb63ae45e9f15203257318cd073a
vikaskumar171020 <vikaskumar171020@gmail.com>
2021-05-23 20:10:45 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function compute() {
     var yearInTheFuture = new Date().getFullYear() + years;
     //Create the Interest text
     document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
-        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "%</mark>,<br/>" +
         "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
         "in the year <mark>" + yearInTheFuture + "</mark>";
 
 }
 
-//update ther ate value
+//update the rate value
 function getSliderValue() {
     document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
 }
