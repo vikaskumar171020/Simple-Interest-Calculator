d67bd99b07abcd34a2c9e21c7aa94a915d3b2cc7
vikaskumar171020 <67593607+vikaskumar171020@users.noreply.github.com>
2021-05-24 00:22:05 +0530

    update css file

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    //Get the values and calculate 
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
+
 }
-        
+
+//update ther Rate value
+function validateRate() {
+    var rate = document.getElementById("rate").value;
+    var biggerThanZero = parseInt(rate) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("rate").focus();
+    }
+
+}
+
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
